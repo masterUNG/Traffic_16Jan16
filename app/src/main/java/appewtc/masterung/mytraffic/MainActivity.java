2978aacd1ebe140d57c33e2a3de23f1c2c1f3b8f
masterUNG <phrombutr@gmail.com>
2016-01-16 14:31:19 +0700

    Bind Widget finish

--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -1,13 +1,30 @@
 package appewtc.masterung.mytraffic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.livTraffic);
+        aboutMeButton = (Button) findViewById(R.id.button);
     }
-}
+
+}   // Main Class นี่คือคลาสหลักเว้ยเห้ย
