8527859ee3d8fd40d98c0a167fee290355e54359
masterUNG <phrombutr@gmail.com>
2016-01-17 15:01:25 +0700

    Success Project

---
+++ app/src/main/java/appewtc/masterung/mytraffic/SplashScreen.java
@@ -0,0 +1,34 @@
+package appewtc.masterung.mytraffic;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Sound Effect
+        MediaPlayer introMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_tata);
+        introMediaPlayer.start();
+
+        //Thread
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 8000);
+
+
+
+    }   // Main Method
+
+}   // Main Class
