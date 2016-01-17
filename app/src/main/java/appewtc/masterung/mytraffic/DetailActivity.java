6a545a4b9634b080c1c98b9c6ef23eb29446ac32
masterUNG <phrombutr@gmail.com>
2016-01-17 14:06:10 +0700

    Show Title & Image finish

--- app/src/main/java/appewtc/masterung/mytraffic/DetailActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/DetailActivity.java
@@ -2,12 +2,44 @@ package appewtc.masterung.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView, detailTextView;
+    private ImageView trafficImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Bind Widget
+        bindWidget();
+
+        //Show View
+        showView();
+
+    }   // Main Method
+
+    private void showView() {
+
+        //Show Title
+        String strTitle = getIntent().getStringExtra("Title");
+        titleTextView.setText(strTitle);
+
+        //Show Image
+        int intIcon = getIntent().getIntExtra("Image", 0);
+        trafficImageView.setImageResource(intIcon);
+
+    }   // showView
+
+    private void bindWidget() {
+        titleTextView = (TextView) findViewById(R.id.textView2);
+        trafficImageView = (ImageView) findViewById(R.id.imageView);
+        detailTextView = (TextView) findViewById(R.id.textView3);
     }
-}
+
+}   // Main Class
--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private void listViewController() {
 
         //1#
-        int[] iconInts = {R.drawable.traffic_01, R.drawable.traffic_02,
+        final int[] iconInts = {R.drawable.traffic_01, R.drawable.traffic_02,
                 R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
                 R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
                 R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11,
@@ -44,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
 
         //#2
-        String[] titleStrings = new String[20];
+        final String[] titleStrings = new String[20];
         titleStrings[0] = "หัวขัอหลัก ที่ 1";
         titleStrings[1] = "หัวขัอหลัก ที่ 2";
         titleStrings[2] = "หัวขัอหลัก ที่ 3";
@@ -73,6 +74,24 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter objMyAdapter = new MyAdapter(getBaseContext(), iconInts,
                 titleStrings, descriptionStrings);
         trafficListView.setAdapter(objMyAdapter);
+
+
+        //Active When Click on ListView
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                Intent objIntent = new Intent(MainActivity.this, DetailActivity.class);
+                objIntent.putExtra("Title", titleStrings[position]);
+                objIntent.putExtra("Image", iconInts[position]);
+                objIntent.putExtra("Index", position);
+                startActivity(objIntent);
+
+
+            }   // event
+        });
+
+
 
 
     }   // listViewController
